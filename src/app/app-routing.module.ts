561050f6581a11b3a37242f1f758e9d5aeae62a7
Jaswanth Chodavarapu <jchodavarapu@incorta.in>
2020-06-21 13:40:45 +0530

    fonts changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ import { FormatComponent } from './pages/format/format.component';
 import { FormatsComponent } from './pages/formats/formats.component';
 import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
-import { ForgotComponent } from './pages/forgot/forgot.component' 
+import { ForgotComponent } from './pages/forgot/forgot.component'
 import { DefaultComponent } from './layouts/default/default.component';
 import { AuthComponent } from './layouts/auth/auth.component';
 
@@ -23,18 +23,18 @@ const routes: Routes = [
     path: '',
     component: DefaultComponent,
     children: [
-      { path: '', component: DashboardComponent, pathMatch: 'full' },
-      { path: 'dashboard', component: DashboardComponent, pathMatch: 'full' },
-      { path: 'devices', component: DevicesComponent, pathMatch: 'full' },
-      { path: 'channels', component: ChannelsComponent, pathMatch: 'full' },
-      { path: 'rules', component: RulesComponent, pathMatch: 'full' },
-      { path: 'rule', component: RuleComponent, pathMatch: 'full' },
-      { path: 'alerts', component: AlertsComponent, pathMatch: 'full' },
-      { path: 'alert', component: AlertComponent, pathMatch: 'full' },
-      { path: 'roles', component: RolesComponent, pathMatch: 'full' },
-      { path: 'users', component: UsersComponent, pathMatch: 'full' },
-      { path: 'format', component: FormatComponent, pathMatch: 'full' },
-      { path: 'formats', component: FormatsComponent, pathMatch: 'full' },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', data: { breadcrumb: 'dashboard' }, component: DashboardComponent, pathMatch: 'full' },
+      { path: 'devices', data: { breadcrumb: 'things / devices' }, component: DevicesComponent, pathMatch: 'full' },
+      { path: 'channels', data: { breadcrumb: 'things / channels' }, component: ChannelsComponent, pathMatch: 'full' },
+      { path: 'rules', data: { breadcrumb: 'rules' }, component: RulesComponent, pathMatch: 'full' },
+      { path: 'rule', data: { breadcrumb: 'rule' }, component: RuleComponent, pathMatch: 'full' },
+      { path: 'alerts', data: { breadcrumb: 'alerts' }, component: AlertsComponent, pathMatch: 'full' },
+      { path: 'alert', data: { breadcrumb: 'alert' }, component: AlertComponent, pathMatch: 'full' },
+      { path: 'roles', data: { breadcrumb: 'confifure / roles' }, component: RolesComponent, pathMatch: 'full' },
+      { path: 'users', data: { breadcrumb: 'confifure / users' }, component: UsersComponent, pathMatch: 'full' },
+      { path: 'format', data: { breadcrumb: 'confifure / format' }, component: FormatComponent, pathMatch: 'full' },
+      { path: 'formats', data: { breadcrumb: 'confifure / formats' }, component: FormatsComponent, pathMatch: 'full' },
     ]
   },
   {
